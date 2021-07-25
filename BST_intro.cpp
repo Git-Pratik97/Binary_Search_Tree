b32040476884ca30c35eb812453de6c9f66bad9b
Pratik Kamble <62785111+Git-Pratik97@users.noreply.github.com>
2021-07-25 12:55:55 +0530

    Add files via upload

---
+++ BST_intro.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val)
+{
+    if (root == NULL)
+    {
+        return new Node(val);
+    }
+
+    if (val < root->data)
+    {
+        root->left = insert(root->left, val);
+    }
+
+    if (val > root->data)
+    {
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+void inorder(Node* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = insert(root, 5);
+    insert(root, 1);
+    insert(root, 3);
+    insert(root, 4);
+    insert(root, 2);
+    insert(root, 7);
+
+    inorder(root);
+    cout << endl;
+
+    return 0;
+
+}
